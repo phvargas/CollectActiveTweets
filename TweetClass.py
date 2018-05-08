fd42cb45a1b4aa8622264e47f437c3455ab7adde
Plinio Vargas <plinio.vargas@verizon.net>
2018-05-08 09:28:04 -0400

    Intital commit

--- TweetClass.py
+++ TweetClass.py
@@ -3,27 +3,22 @@ import sys
 import re
 import twitter
 import time as timeit
+import datetime
 import pickle
 import gzip
+import json
 from twitter_apps.TwitterFunctions import TObject
+from Utilities.ProgressBar import display_progress_bar
 
 
 class TweetClass:
-    def __init__(self, path_tweet, auth='male'):
-        self.path = path_tweet
+    def __init__(self, auth='males'):
         self.handles = []
         self.auth = auth
         self.suffix = '.twt.gz'
-
-        if os.path.isdir(path_tweet):
-            self.load_tweets_files()
-
-        else:
-            print("Could not find tweet folder: {}".format(path_tweet), file=sys.stderr)
-            exit(-1)
-
-        self.handle_with_tweets = len(self.handles)
+        self.wait_time = 60 * 15
         self.tweetObject = TObject(self.auth)
+        self.end_time = datetime.datetime.now() + datetime.timedelta(minutes=16) + datetime.timedelta(seconds=10)
 
     def load_tweets_files(self,):
         dir_files = os.listdir(self.path)
@@ -39,11 +34,16 @@ class TweetClass:
         self.handles.sort()
 
     def get_tweets(self, handle, max_count=3200):
+        if max_count < 200:
+            default_count = max_count
+        else:
+            default_count = 200
+
         handle_tweets = []
         print("Getting tweets for {}".format(handle), file=sys.stderr)
 
         # make initial request for most recent tweets (200 is the maximum allowed count)
-        new_tweets = self.retrieve_tweet_blocks(handle)
+        new_tweets = self.retrieve_tweet_blocks(handle, default_count)
         for tweet in new_tweets:
             handle_tweets.append(tweet)
 
@@ -57,11 +57,16 @@ class TweetClass:
         # keep grabbing tweets until there are no tweets left to grab
         while 0 < len(handle_tweets) < max_count:
             # all subsequent requests use the max_id param to prevent duplicates
-            new_tweets = self.retrieve_tweet_blocks(handle, 200, oldest)
+            if max_count == 3200 or (max_count - len(handle_tweets)) > 200:
+                default_count = 200
+            else:
+                default_count = max_count - len(handle_tweets)
+
+            new_tweets = self.retrieve_tweet_blocks(handle, default_count, oldest)
 
             for tweet in new_tweets:
                 handle_tweets.append(tweet)
-            print('{:,} tweets...'.format(len(handle_tweets)))
+            display_progress_bar(20, len(handle_tweets)/max_count)
 
             # update the id of the oldest tweet less one
             if len(new_tweets) > 0:
@@ -75,35 +80,84 @@ class TweetClass:
         have_to_wait = True
         tweet_block = []
 
+        wait_time = self.wait_time
         while have_to_wait:
             try:
                 tweet_block = self.tweetObject.api.GetUserTimeline(screen_name=screen_name, count=count,  max_id=max_id)
                 have_to_wait = False
             except twitter.error.TwitterError as e:
                 print(screen_name, 'message:', e.message)
-                if 'code' in e.message[0]:
-                    print(e.message[0]['code'])
-                else:
-                    print('no code in msg')
 
                 if e.message == 'Not authorized.' or 'Unknown error: ' in e.message or e.message[0]['code'] == 34:
                     print('Account deleted or not authorized... Moving on ... ')
                     return []
 
-                print('We have to wait 15 mins.')
-                timeit.sleep(61 * 15)
+                if e.message and len(e.message) > 0 and 'code' in e.message[0]:
+                    if e.message[0]['code'] == 88:
+                        wait_time = (self.end_time - datetime.datetime.now()).seconds
+
+                print('Waiting {:.2f} minutes. Re-starting at: {}'.format(wait_time / 60,
+                                                                          self.end_time.strftime("%H:%M:%S")))
+                timeit.sleep(wait_time)
+                self.reset_wait_time()
 
         return tweet_block
+
+    def get_friends(self, screen_name):
+        have_to_wait = True
+        result = []
+
+        wait_time = self.wait_time
+        while have_to_wait:
+            try:
+
+                for friend in self.tweetObject.api.GetFriends(screen_name=screen_name, count=200):
+                    result.append(friend)
+                have_to_wait = False
+            except twitter.error.TwitterError as e:
+                print(screen_name, 'message:', e.message)
+
+                if e.message == 'Not authorized.' or 'Unknown error: ' in e.message or e.message[0]['code'] == 34:
+                    print('Account deleted or not authorized... Moving on ... ')
+                    return []
+
+                if e.message and len(e.message) > 0 and 'code' in e.message[0]:
+                    if e.message[0]['code'] == 88:
+                        wait_time = (self.end_time - datetime.datetime.now()).seconds
+
+                print('Waiting {:.2f} minutes. Re-starting at: {}'.format(wait_time / 60,
+                                                                          self.end_time.strftime("%H:%M:%S")))
+                timeit.sleep(wait_time)
+                self.reset_wait_time()
+
+        return result
+
+    def reset_wait_time(self):
+        self.end_time = datetime.datetime.now() + datetime.timedelta(minutes=16) + datetime.timedelta(seconds=10)
 
     def save(self, handle, _data):
         """
         save objects into a compressed diskfile
         :param
         """
-        filename = self.path + handle + self.suffix
+        #filename = self.path + handle + self.suffix
+        filename = handle + self.suffix
 
         with gzip.open(filename, mode='wb') as f:
             pickle.dump(_data, f, pickle.HIGHEST_PROTOCOL)
+
+    def json_save(self, handle, _data):
+        """
+        save objects into a compressed diskfile
+        :param
+        """
+        json_str = json.dumps(_data) + "\n"
+        json_bytes = json_str.encode('utf-8')
+
+        filename = self.path + handle + self.suffix
+
+        with gzip.open(filename, mode='wb') as f:
+            f.write(json_bytes)
 
     def load(self, handle):
         """
